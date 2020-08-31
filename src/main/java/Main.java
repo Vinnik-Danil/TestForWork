c835a1d6dbb52bf510ac57571b9f74be46a9543d
Vinnik-Danil <58749648+Vinnik-Danil@users.noreply.github.com>
2020-08-31 11:03:26 +0300

    !

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello word");
+        Scanner scanner = new Scanner(System.in);
+        int N,K,result;
+        N=scanner.nextInt();
+        K=scanner.nextInt();
+
+        System.out.println(result = (getFactorial(N))/(getFactorial(K)*getFactorial(N-K)));
+    }
+
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
     }
 }
